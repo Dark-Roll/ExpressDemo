5e5bd083b38e99c9d00f1481ae99e1dd94cbff0f
Willy Yang <willy.yang@bonny-live.com>
2019-10-09 16:26:07 +0800

    Add: create and update function

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,81 @@ router.get('/', function(req, res, next) {
 
 });
 
+// add page
+router.get('/add', function(req, res, next) {
+
+  // use userAdd.ejs
+  res.render('userAdd', { title: 'Add User'});
+});
+
+
+// add post
+router.post('/userAdd', function(req, res, next) {
+
+  var db = req.con;
+
+  var sql = {
+      id: req.body.id,
+      people: req.body.people,
+      group_name: req.body.group_name
+  };
+
+  //console.log(sql);
+  var qur = db.query('INSERT INTO activity SET ?', sql, function(err, rows) {
+      if (err) {
+          console.log(err);
+      }else{
+        console.log("maybe insert success");
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.redirect('/');
+  });
+
+});
+
+// edit page
+router.get('/userEdit', function(req, res, next) {
+
+	var id = req.query.id;
+	var db = req.con;
+	var data = "";
+
+	// 若要取得網址列參數，可使用 req.query.[參數]
+
+	db.query('SELECT * FROM activity WHERE id = ?', id, function(err, rows) {
+			if (err) {
+					console.log(err);
+			}
+			console.log(rows);
+			var data = rows;
+			res.render('userEdit', { title: 'Edit Account', activities: data });
+	});
+
+});
+
+router.post('/userEdit', function(req, res, next) {
+
+	var db = req.con;
+	var id = req.body.id;
+
+	var sql = {
+			id: req.body.id,
+			people: req.body.people,
+			group_name: req.body.group_name
+	};
+
+
+	// string 可過
+	// ? 是被變數取代的樣子
+	var qur = db.query('UPDATE activity SET ? WHERE id = ?', [sql, id], function(err, rows) {
+			if (err) {
+					console.log(err);
+			}
+
+			res.setHeader('Content-Type', 'application/json');
+			res.redirect('/');
+	});
+
+});
+
 module.exports = router;
